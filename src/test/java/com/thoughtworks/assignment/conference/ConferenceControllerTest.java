7b40ccd85841ee4abc420f21ffa061a72b731901
leandro <leandroleo02@gmail.com>
2016-06-26 01:56:02 -0300

    Added test for empty files entry

--- src/test/java/com/thoughtworks/assignment/conference/ConferenceControllerTest.java
+++ src/test/java/com/thoughtworks/assignment/conference/ConferenceControllerTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.assignment.conference;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -21,14 +22,26 @@ public class ConferenceControllerTest {
 	}
 
 	@Test
-	public void testWithDefaultFile() throws IOException {
+	public void testWithDefaultProposalsTalks() throws IOException {
+		List<String> talks = readFile("testProposalsTalks.txt");
+		List<Track> trackList = controller.getTrackList(talks);
+		assertThat("Tracks should have size 2", trackList.size(), equalTo(2));
+	}
+
+	@Test
+	public void testWithNoTalks() throws IOException {
+		List<String> talks = readFile("testEmptyTalks.txt");
+		List<Track> trackList = controller.getTrackList(talks);
+		assertThat(trackList.size(), equalTo(0));
+	}
+
+	private List<String> readFile(String fileName) throws FileNotFoundException, IOException {
 		ClassLoader classLoader = getClass().getClassLoader();
-		String file = classLoader.getResource("testProposalsTalks.txt").getFile();
+		String file = classLoader.getResource(fileName).getFile();
 
 		InputFileReader reader = new InputFileReader();
 
 		List<String> talks = reader.readFile(file);
-		List<Track> trackList = controller.getTrackList(talks);
-		assertThat("Tracks should have size 2", trackList.size(), equalTo(2));
+		return talks;
 	}
 }
